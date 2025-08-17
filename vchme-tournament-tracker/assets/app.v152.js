33994a3d25a0b8b9b9afdfbccde39f27abfeecdc
VCHME <vc.haslau.maria.ellend@gmail.com>
2025-08-17 19:17:29 +0200

    Delete vchme-tournament-tracker directory

--- vchme-tournament-tracker/assets/app.v152.js
+++
@@ -1,5 +0,0 @@
-(function(){'use strict';
-function $ (s,c){return (c||document).querySelector(s)}
-function $$ (s,c){return Array.prototype.slice.call((c||document).querySelectorAll(s))}
-...
-})();
